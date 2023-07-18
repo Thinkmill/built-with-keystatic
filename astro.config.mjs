bd75ce15b850a218fc6c02d597f42b6f35650013
Simon Vrachliotis <simon@simonswiss.com>
2023-07-18 16:52:22 +1000

    WIP SEO tags

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,11 @@
+import { loadEnv } from "vite";
 import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
 import { astroImageTools } from "astro-imagetools";
 
 import markdoc from "@astrojs/markdoc";
+
+const { SITE_URL } = loadEnv(import.meta.env.MODE, process.cwd(), "");
 
 // https://astro.build/config
 export default defineConfig({
@@ -11,4 +14,5 @@ export default defineConfig({
   experimental: {
     assets: true,
   },
+  site: SITE_URL,
 });
