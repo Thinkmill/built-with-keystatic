ce7226ac621ea33109009e99e86be1f864859387
Simon Vrachliotis <simon@simonswiss.com>
2023-07-19 09:02:59 +1000

    Add Astro Vercel adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,17 +2,20 @@ import { loadEnv } from "vite";
 import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
 import { astroImageTools } from "astro-imagetools";
-
 import markdoc from "@astrojs/markdoc";
+import vercel from "@astrojs/vercel/serverless";
+const {
+  SITE_URL
+} = loadEnv(import.meta.env.MODE, process.cwd(), "");
 
-const { SITE_URL } = loadEnv(import.meta.env.MODE, process.cwd(), "");
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [react(), markdoc(), astroImageTools],
   output: "hybrid",
   experimental: {
-    assets: true,
+    assets: true
   },
   site: SITE_URL,
+  adapter: vercel()
 });
