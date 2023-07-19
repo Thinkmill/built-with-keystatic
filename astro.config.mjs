90404b0f61fd6acd03601d2bd6da2bb36c8ca950
Simon Vrachliotis <simon@simonswiss.com>
2023-07-19 11:10:46 +1000

    Fix og tags and remove env variable for SITE_URL

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,21 +1,14 @@
-import { loadEnv } from "vite";
 import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
 import markdoc from "@astrojs/markdoc";
 import vercel from "@astrojs/vercel/serverless";
 
-// Grabbing the SITE_URL from the environment variables 
-// (used for SEO metatags base path)
-const {
-  SITE_URL
-} = loadEnv(import.meta.env.MODE, process.cwd(), "");
-
 export default defineConfig({
   integrations: [react(), markdoc()],
   output: "hybrid",
   experimental: {
-    assets: true
+    assets: true,
   },
-  site: SITE_URL,
-  adapter: vercel()
+  site: "https://builtwith.keystatic.com",
+  adapter: vercel(),
 });
